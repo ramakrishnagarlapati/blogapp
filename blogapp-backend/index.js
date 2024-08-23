05492918d752f6072dc0e13a3fc65fe9ff5fc9b2
Rama krishna <ramakrishnagarlapati04@gmail.com>
2024-08-23 17:57:48 +0530

    Initial commit Set up backend and frontend folder structure

---
+++ blogapp-backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
