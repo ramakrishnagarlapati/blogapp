fa605938c9f1bd5039cc2fad669d1b1e8ba702d3
Rama krishna <ramakrishnagarlapati04@gmail.com>
2024-08-23 18:05:18 +0530

    Database set up, created posts table, and inserted dummy data if empty

---
+++ blogapp-backend/database.js
@@ -0,0 +1,44 @@
+const sqlite3 = require("sqlite3").verbose();
+//Create a new database
+const db = new sqlite3.Database("./blog.db");
+
+db.serialize(() => {
+  //Create Posts table if not already exists
+  db.run(`CREATE TABLE IF NOT EXISTS posts(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT,
+          content TEXT,
+          created_at DATETIME DEFAULT CURRENT_TIMESTAMP 
+          )`);
+
+  //Get number of entries in posts table to check if it is empty
+  db.get("SELECT COUNT(*) AS count FROM posts", (err, res) => {
+    if (err) {
+      console.error(err.message);
+    }
+    //If posts table is empty, Insert some dummy data
+    if (res.count === 0) {
+      db.run(`INSERT INTO posts (title,content) VALUES
+              ("His mother had always taught him","His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind."),
+              ("He was an expert but not in a discipline","He was an expert but not in a discipline that anyone could fully appreciate. He knew how to hold the cone just right so that the soft server ice-cream fell into it at the precise angle to form a perfect cone each and every time. It had taken years to perfect and he could now do it without even putting any thought behind it."),
+              ("Dave watched as the forest burned up on the hill.","Dave watched as the forest burned up on the hill, only a few miles from her house. The car had been hastily packed and Marta was inside trying to round up the last of the pets. Dave went through his mental list of the most important papers and documents that they couldn't leave behind. He scolded himself for not having prepared these better in advance and hoped that he had remembered everything that was needed. He continued to wait for Marta to appear with the pets, but she still was nowhere to be seen."),
+              ("All he wanted was a candy bar.","All he wanted was a candy bar. It didn't seem like a difficult request to comprehend, but the clerk remained frozen and didn't seem to want to honor the request. It might have had something to do with the gun pointed at his face."),
+              ("Hopes and dreams were dashed that day.","Hopes and dreams were dashed that day. It should have been expected, but it still came as a shock. The warning signs had been ignored in favor of the possibility, however remote, that it could actually happen. That possibility had grown from hope to an undeniable belief it must be destiny. That was until it wasn't and the hopes and dreams came crashing down."),
+              ("Dave wasn't exactly sure how he had ended up","Dave wasn't exactly sure how he had ended up in this predicament. He ran through all the events that had lead to this current situation and it still didn't make sense. He wanted to spend some time to try and make sense of it all, but he had higher priorities at the moment. The first was how to get out of his current situation of being naked in a tree with snow falling all around and no way for him to get down."),
+              ("This is important to remember.","This is important to remember. Love isn't like pie. You don't need to divide it among all your friends and loved ones. No matter how much love you give, you can always give more. It doesn't run out, so don't try to hold back giving it as if it may one day run out. Give it freely and as much as you want."),
+              ("One can cook on and with an open fire.","One can cook on and with an open fire. These are some of the ways to cook with fire outside. Cooking meat using a spit is a great way to evenly cook meat. In order to keep meat from burning, it's best to slowly rotate it."),
+              ("There are different types of secrets.","There are different types of secrets. She had held onto plenty of them during her life, but this one was different. She found herself holding onto the worst type. It was the type of secret that could gnaw away at your insides if you didn't tell someone about it, but it could end up getting you killed if you did."),
+              ("They rushed out the door.","They rushed out the door, grabbing anything and everything they could think of they might need. There was no time to double-check to make sure they weren't leaving something important behind. Everything was thrown into the car and they sped off. Thirty minutes later they were safe and that was when it dawned on them that they had forgotten the most important thing of all."),
+              ("It wasn't quite yet time to panic.","It wasn't quite yet time to panic. There was still time to salvage the situation. At least that is what she was telling himself. The reality was that it was time to panic and there wasn't time to salvage the situation, but he continued to delude himself into believing there was."),
+              ("She was aware that things could go wrong.","She was aware that things could go wrong. In fact, she had trained her entire life in anticipation that things would go wrong one day. She had quiet confidence as she started to see that this was the day that all her training would be worthwhile and useful. At this point, she had no idea just how wrong everything would go that day."),
+              ("She wanted rainbow hair.","She wanted rainbow hair. That's what she told the hairdresser. It should be deep rainbow colors, too. She wasn't interested in pastel rainbow hair. She wanted it deep and vibrant so there was no doubt that she had done this on purpose."),
+              ("The paper was blank.","The paper was blank. It shouldn't have been. There should have been writing on the paper, at least a paragraph if not more. The fact that the writing wasn't there was frustrating. Actually, it was even more than frustrating. It was downright distressing."),
+              ("The trees, therefore, must be such old","The trees, therefore, must be such old and primitive techniques that they thought nothing of them, deeming them so inconsequential that even savages like us would know of them and not be suspicious. At that, they probably didn't have too much time after they detected us orbiting and intending to land. And if that were true, there could be only one place where their civilization was hidden."),
+              ("There was only one way to do things in the Statton house.","There was only one way to do things in the Statton house. That one way was to do exactly what the father, Charlie, demanded. He made the decisions and everyone else followed without question. That was until today."),
+              ("She was in a hurry.","She was in a hurry. Not the standard hurry when you're in a rush to get someplace, but a frantic hurry. The type of hurry where a few seconds could mean life or death. She raced down the road ignoring speed limits and weaving between cars. She was only a few minutes away when traffic came to a dead standstill on the road ahead.")
+              `);
+    }
+  });
+});
+
+module.exports = db;
