181a6db6b8c16a38325444cb8389538d5ae3cc34
Rama krishna <ramakrishnagarlapati04@gmail.com>
2024-08-25 12:01:37 +0530

    create not found component to display error page when user types invalid route

--- blogapp-frontend/src/App.js
+++ blogapp-frontend/src/App.js
@@ -1,8 +1,11 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
+
 import Home from "./pages/Home";
 import PostDetail from "./pages/PostDetail";
 import PostEdit from "./pages/PostEdit";
 import NewPost from "./pages/NewPost";
+import NotFound from "./pages/NotFound";
+
 import "./App.css";
 
 function App() {
@@ -20,6 +23,12 @@ function App() {
 
         {/*Route for editing an existing post */}
         <Route exact path="/posts/:postId/edit" component={PostEdit} />
+
+        {/*Route to handle invalid routes */}
+        <Route exact path="/not-found" component={NotFound} />
+
+        {/*If url does not match with any of the above, redirect to /not-found */}
+        <Redirect to="/not-found" />
       </Switch>
     </div>
   );
